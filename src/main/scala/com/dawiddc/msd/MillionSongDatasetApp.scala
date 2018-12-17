aa7ab36931f38f54d7cc7fd95552f85c7914630b
dawiddc <dawid.ciok@gmail.com>
2018-12-18 00:53:48 +0100

    Finish functional part

--- src/main/scala/com/dawiddc/msd/MillionSongDatasetApp.scala
+++ src/main/scala/com/dawiddc/msd/MillionSongDatasetApp.scala
@@ -17,7 +17,7 @@ object MillionSongDatasetApp {
     val spark = SparkSession
       .builder()
       .appName("Million Song Dataset Sql")
-      .config(new SparkConf().setAppName("Million Song Dataset App").setMaster("local[*]"))
+      .config(new SparkConf().setAppName("Million Song Dataset App").setMaster("local[4]"))
       .getOrCreate()
     import spark.implicits._
 
@@ -37,10 +37,10 @@ object MillionSongDatasetApp {
     topTracks.select("title", "artist", "count").sort($"count".desc).show()
 
     println("2. Top 10 users by distinct songs played:")
-    songLogDF.distinct().groupBy("userId").count().sort($"count".desc).show(10)
+    songLogDF.dropDuplicates("trackId", "userId").groupBy("userId").count().sort($"count".desc).show(10)
 
-    println("3. Top 10 most popular artists:")
-    val joinedDF = songLogDF.join(trackInfoDF, usingColumns = Seq("trackId"))
+    println("3. Most popular artist:")
+    val joinedDF = songLogDF.join(trackInfoDF.dropDuplicates("trackId"), usingColumns = Seq("trackId"))
     joinedDF.groupBy("artist").count().sort($"count".desc).show(1)
 
     println("4. Songs listened by month:")
@@ -51,6 +51,24 @@ object MillionSongDatasetApp {
     val parseTimestampToMonth = udf(format)
     val songsWithMonthDF = songLogDF.withColumn("listenMonth", parseTimestampToMonth(songLogDF("listenDate")))
     songsWithMonthDF.groupBy("listenMonth").count().sort($"listenMonth").show(12)
+
+    println("5. Number of users who listened to top3 Queen songs:")
+    val top3tracks = joinedDF.filter($"artist" === "Queen")
+      .groupBy("trackId")
+      .count().select("trackId").sort($"count".desc).limit(3).map(r => r.getString(0)).collect.toList
+
+    val queenTop1listeners = joinedDF.filter($"trackId" === top3tracks(0)).select("userId").distinct()
+    val queenTop1listenersList = queenTop1listeners.map(r => r.getString(0)).collect.toList
+    val queenTop2listeners = joinedDF
+      .filter($"trackId" === top3tracks(1) && $"userId".isin(queenTop1listenersList: _*))
+      .select("userId").distinct()
+    val queenTop2listenersList = queenTop2listeners.map(r => r.getString(0)).collect.toList
+    val queenTop3listeners = joinedDF
+      .filter($"trackId" === top3tracks(2) && $"userId".isin(queenTop2listenersList: _*))
+      .select("userId").distinct()
+
+    queenTop3listeners.sort($"userId").show(10)
+    println("Number of users:" + queenTop3listeners.count())
 
     val totalTime = System.currentTimeMillis - start
     println("Elapsed time: %1d ms".format(totalTime))
